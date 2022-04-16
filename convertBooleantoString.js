9feb8625ccd72605a2bfed7c49b0725bd691f84a
MarkoSOE <markoalhamadani@gmail.com>
2022-04-16 14:25:53 -0400

    added convertBooleantoStrings.js

---
+++ convertBooleantoString.js
@@ -0,0 +1,17 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+    //using a ternary operator that evaluates the condition to the left of the "?" if it's true then the middle statement is returned, if false then the last statement is returned
+    return b ? "true" : "false" 
+  }
+
+
+
+//   Alternative answer: 
+
+// function b(bb){
+//     return bb.toString();
+// }

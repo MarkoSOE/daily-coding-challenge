7177740b5dd9b60df735d0ca515ba4d6a5f9ba68
MarkoSOE <markoalhamadani@gmail.com>
2022-04-23 01:13:33 -0400

    added rentalCarCost.js

---
+++ returningStrings.js
@@ -0,0 +1,6 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    //using template literals to return the name provided
+    return `Hello, ${name} how are you doing today?`
+  }

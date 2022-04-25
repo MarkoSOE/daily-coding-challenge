fd0b2dbf5356a1cf1c746bf131d1359430b39c93
MarkoSOE <markoalhamadani@gmail.com>
2022-04-24 23:40:42 -0400

    added stringEndWith.js

---
+++ stringEndWith.js
@@ -0,0 +1,6 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string)
+
+function solution(str, ending){
+    // using endswith string method to return true if a string (str) ends with a specified string (ending)
+    return str.endsWith(ending)
+  }

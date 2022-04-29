a4679fcdf437a679352159f5a8daf1ca24d00d08
MarkoSOE <markoalhamadani@gmail.com>
2022-04-28 23:18:32 -0400

    added complementaryDNA.js

---
+++ complementaryDNA.js
@@ -0,0 +1,9 @@
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+    return dna.split(``).map(function(v){
+        return pairs[v]
+    }).join("");
+}

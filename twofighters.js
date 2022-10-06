91a0252577f01d5099444688ec12a10ce1788e0c
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-10-05 22:52:15 -0400

    added twofighters.js

--- twofighters.js
+++ twofighters.js
@@ -20,7 +20,49 @@
 //         this.damagePerAttack = damagePerAttack;
 //         this.toString = function() { return this.name; }
 // }
+function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
+}
 
 function declareWinner(fighter1, fighter2, firstAttacker) {
-    return "Write your code here";
+    
+    if(firstAttacker === fighter1.name){
+        while(fighter1.health - fighter2.damagePerAttack > 0 || fighter2.health - fighter1.damagePerAttack > 0){
+            fighter2.health -= fighter1.damagePerAttack
+            console.log(`${firstAttacker} attacks ${fighter2.name}; ${fighter2.name} now has ${fighter2.health} health`)
+            //fighter 2 attacks fighter 1
+            fighter1.health -= fighter2.damagePerAttack
+            console.log(`${fighter2.name} attacks ${fighter1.name}; ${fighter1.name} now has ${fighter1.health} health`)
+            if(fighter1.health - fighter2.damagePerAttack <= 0){
+                fighter1.health -= fighter2.damagePerAttack
+                console.log(`${fighter2.name} attacks ${fighter1.name}: ${fighter1.name} now has ${fighter1.health} and is dead. ${fighter2.name} wins`)
+                return fighter2.name
+            }
+            else if(fighter2.health - fighter1.damagePerAttack <= 0){
+                fighter2.health -= fighter1.damagePerAttack
+                console.log(`${fighter1.name} attacks ${fighter2.name}: ${fighter2.name} now has ${fighter2.health} and is dead. ${fighter1.name} wins`)
+                return fighter1.name
+            }
+        }
+    }
+  }
+
+  
+
+//couldnt finish the question; solution attached below: 
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    while (fighter1.health > 0 && fighter2.health > 0) {
+      fighter2.health -= fighter1.damagePerAttack;
+      fighter1.health -= fighter2.damagePerAttack;
+    }
+    
+    if (fighter1.health <= 0 && fighter2.health <= 0)
+      return firstAttacker;
+    else if (fighter1.health <= 0)
+      return fighter2.name;
+    else
+      return fighter1.name;
   }
